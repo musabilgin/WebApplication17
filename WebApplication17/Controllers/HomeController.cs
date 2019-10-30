96f7a8bc65865c8edf6f68662c0c81058e01c38f
Musa Bilgin <musa_bilgin_55@hotmail.com>
2019-10-30 14:40:39 +0300

    Test

--- WebApplication17/Controllers/HomeController.cs
+++ WebApplication17/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace WebApplication17.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application descffription page.";
+            ViewData["Message"] = "Your applidffdgdfgfdgcation descffription page.";
 
             return View();
         }
